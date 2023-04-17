8dc3d4e69402b72eba67e5df402e66fe13f50f14
Gavin Crowley <gavincrowley@gmail.com>
2023-04-17 20:39:35 +0100

    April 17

---
+++ BeginnersGuide/src/MoonWeight.java
@@ -0,0 +1,10 @@
+public class MoonWeight {
+    public static void main(String[] args) {
+        double weight = 90;
+        double effectiveWeight;
+
+        effectiveWeight = weight * 0.83;
+        System.out.println("My weight on the moon would be "
+                + effectiveWeight + " Kgs.");
+    }
+}
---
+++ BeginnersGuide/src/Sandbox.java
@@ -0,0 +1,12 @@
+public class Sandbox {
+    public static void main(String[] args) {
+//        int prime = 2;
+        for (int i = 2; i < 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println("");
+            }
+            System.out.println(i);
+        }
+//        System.out.println(sum);
+    }
+}
